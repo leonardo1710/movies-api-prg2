d8b718d99358e832da2583a94ca0c3f463061e2f
leonardo1710 <leon.freudenthaler@fh-campuswien.ac.at>
2025-02-03 16:25:45 +0100

    commit 2025

--- src/main/java/at/ac/fhcampuswien/moviesapi/MoviesApiApplication.java
+++ src/main/java/at/ac/fhcampuswien/moviesapi/MoviesApiApplication.java
@@ -1,9 +1,22 @@
 package at.ac.fhcampuswien.moviesapi;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class MoviesApiApplication {
 
     public static void main(String[] args) {
@@ -11,4 +24,29 @@ public class MoviesApiApplication {
         SpringApplication.run(MoviesApiApplication.class, args);
     }
 
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        // Allow all origins, headers, and methods
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().addServersItem(new Server().url("https://prog2.fh-campuswien.ac.at/"))
+                .addServersItem(new Server().url("http://localhost:8080/"))
+                .components(new Components().addSecuritySchemes("basicScheme",
+                        new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("basic")))
+                .info(new Info().title("Movie API").version("V1").description("Movie API for PRG2 CSDC")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+    }
 }
